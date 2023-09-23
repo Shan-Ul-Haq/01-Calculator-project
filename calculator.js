dfbc7b3de0defa3b9db38e2746d802faded706a9
Shan-Ul-Haq <shanulhaq84@gmail.com>
2023-09-23 16:40:03 +0500

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import { add } from "./add.js";
+import { subtraction } from "./subtraction.js";
+import { multiply } from "./multiply.js";
+import { divide } from "./divide.js";
+import { modulus } from "./modulus.js";
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        message: "Enter your first number",
+        type: "number",
+        name: "num1"
+    },
+    { message: "Enter your second number",
+        type: "number",
+        name: "num2"
+    },
+    { message: "Select Operator",
+        type: "list",
+        choices: ["+", "-", "*", "/", "%"],
+        name: "operator"
+    }]);
+if (answer.operator === "+") {
+    let result = add(answer.num1, answer.num2);
+    console.log(`add ${answer.num1} + ${answer.num2} = ${result}`);
+}
+else if (answer.operator === "-") {
+    let result = subtraction(answer.num1, answer.num2);
+    console.log(`subtraction ${answer.num1} - ${answer.num2} = ${result}`);
+}
+else if (answer.operator === "*") {
+    let result = multiply(answer.num1, answer.num2);
+    console.log(`multiply ${answer.num1} * ${answer.num2} = ${result}`);
+}
+else if (answer.operator === "/") {
+    let result = divide(answer.num1, answer.num2);
+    console.log(`divide ${answer.num1} / ${answer.num2} = ${result}`);
+}
+else if (answer.operator === "%") {
+    let result = modulus(answer.num1, answer.num2);
+    console.log(`modulus ${answer.num1} % ${answer.num2} = ${result}`);
+}
---
+++ subtraction.js
@@ -0,0 +1,3 @@
+export function subtraction(num1, num2) {
+    return num1 - num2;
+}
